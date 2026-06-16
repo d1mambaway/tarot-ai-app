d550e23c3162b3fe9d2bda3b2a200e8e752c8e05
d1mambaway <d1mambaway@users.noreply.github.com>
2026-06-16 11:47:02 +0000

    Fix: remove next-intl from next.config.js (was causing build failure)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-const createNextIntlPlugin = require('next-intl/plugin');
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Telegram Mini Apps require no trailing slashes
   trailingSlash: false,
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
